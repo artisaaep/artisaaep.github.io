3ae051ec497424996dfe6ac541906ccccf442990
artisaaep <104840798+artisaaep@users.noreply.github.com>
2024-06-21 17:37:41 +0300

    trip created page finished

--- tripcreated.js
+++ tripcreated.js
@@ -1,14 +1,19 @@
-var clickActive = false;
-var waitTime = 150;
-
-
-document.getElementById("car").onclick = function() {
-  if(!clickActive) {
-    clickActive = true;
-    this.style.animation = "getSmaller "+waitTime+"ms";
-    setTimeout(()=>{
-      this.style.animation = "";
-      clickActive = false;
-    }, waitTime);
-  }
+//window.Telegram.WebApp.showAlert("hi");
+let cnt = 0;
+window.onload = inv;
+function inv() {
+    const body = document.body;
+    const computedStyle = getComputedStyle(body);
+    if (computedStyle.backgroundColor === 'rgb(28, 28, 29)') {
+        document.getElementById('car').style.filter = 'invert(1)';
+    }
 }
+Telegram.WebApp.onEvent('themeChanged', function() {
+    const body = document.body;
+    const computedStyle = getComputedStyle(body);
+    if (computedStyle.backgroundColor === 'rgb(28, 28, 29)') {
+        document.getElementById('car').style.filter = 'invert(1)';
+    } else {
+        document.getElementById('car').style.filter = 'none';
+    }
+});
