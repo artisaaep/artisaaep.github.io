65a541de8b97bb1880f0ad2c6373c5874131c415
artisaaep <104840798+artisaaep@users.noreply.github.com>
2024-06-23 00:19:05 +0300

    button links added

--- links.js
+++ links.js
@@ -1,6 +1,11 @@
 function myprofile() {
     window.location.href = 'profile.html';
 }
+
 function goBack() {
     window.history.back();
+}
+
+function ride() {
+    window.location.href = 'notification_of_the_application_to_the_driver.html';
 }
